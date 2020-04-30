22b7606291da526ce78355e646d1e9dd074dfbfe
Edoardo Pirovano <edoardo.pirovano@gmail.com>
2020-04-30 11:41:57 +0100

    Add support for colour Sudoku.

--- sudoku_z3.py
+++ sudoku_z3.py
@@ -2,10 +2,10 @@
 
 from sudoku import Sudoku
 
-def z3_solving(sudoku):
+def z3_solving(sudoku, colours):
     """ Function solving the given sudoku puzzle using Z3 """
 
-    from z3 import Solver, Int, Or, Distinct, sat
+    from z3 import Solver, Int, Or, Sum, Distinct, sat
 
     #positions = cross("ABCDEFGHI", "123456789")
     symbols = {pos: Int(pos) for pos in sudoku.positions}
@@ -35,6 +35,13 @@ def z3_solving(sudoku):
         if value in "123456789":
             s.add(symbols[pos] == value)
 
+    for colour in colours:
+        blobs = []
+        for blob in colour:
+            blobs.append(Sum([symbols[blob[i:i+2]] for i in range(0, len(blob), 2)]))
+        for blob in blobs[1:]:
+            s.add(blob == blobs[0])
+
     if s.check() != sat:
         raise Exception("unsolvable")
 
@@ -49,16 +56,32 @@ def main(puzzle):
 
     print("[+] start solving using Z3")
 
-    s_solved = z3_solving(s)
+    s_solved = z3_solving(s, [])
 
     print("[+] solved:", s_solved.is_solved())
     print(s_solved)
 
+def colour(puzzle):
+    print("[+] parsing colour puzzle:", puzzle)
+
+    colours = []
+    for colour in puzzle.split(";"):
+        colours.append(colour.split(","))
+
+    print("[+] start solving using Z3")
+
+    s_solved = z3_solving(Sudoku(), colours)
+
+    print("[+] solved")
+    print(Sudoku.parse(str(s_solved)).pretty_print())
+
 if __name__ == "__main__":
     from sys import argv
 
-    if len(argv) != 2:
-        print("[!] we are using some test puzzle due to missing argument")
-        main("4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......")
-    else:
+    if len(argv) == 2:
         main(argv[1])
+    elif (argv[1] == "-c"):
+        colour(argv[2])
+    else:
+        print("[!] we are using some test puzzle due to invalid arguments")
+        main("4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......")
